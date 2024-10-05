d2d976488a1550d4ef5c096752cecdc3a927070f
Abbas Jivan <abbasjivan04@gmail.com>
2024-10-05 11:53:49 -0400

    commit

--- app/backend/chat.py
+++ app/backend/chat.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from IPython.display import Image, display, Audio, Markdown
+import base64
 
 load_dotenv()
 
@@ -13,13 +15,38 @@ client = OpenAI(
   api_key=open_ai_api_key
 )
 
+def process_image(image_url):
+    with open(image_url, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+    
+def image_splicer(images):
+    return None
+    
+def create_story(age, images):
+    #storyboard = image_splicer(images)
+    image = process_image(images)
 
-completion = client.chat.completions.create(
-  model=model,
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant. Help me with my math homework!"}, # <-- This is the system message that provides context to the model
-    {"role": "user", "content": "Hello! Could you solve 2+2?"}  # <-- This is the user message for which the model will generate a response
-  ]
-)
+    messages = [
+        {'role': 'system', 'content': "You are writing children's stories for young children based on drawing that they give you. Please use the appropiate vocabulary and only output the story you make."},
+        {'role': 'user', 'content': [
+            {'type': 'text', 'text': 'Based on these images, can you create a story that is easy for a 5 year old to read and will help me them to learn how to get better at reading. Note that the black bars represent different frames of the story. Note that the vocabulary used should be appropriate for a 5 year old to understand. Only output the story and nothing else.'},
+            {'type': 'image_url', 'image_url': {
+                'url': f"data:image/png;base64, {image}"
+            }}
+        ]}
+    ]
 
-print(completion.choices[0].message.content)
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.7
+    )
+
+    return response.choices[0].message.content
+
+
+print(create_story(5, "assets/combined_image.png"))
+
+
+
+
