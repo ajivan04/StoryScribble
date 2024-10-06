fe77677724987288e4009295030e29163543d54f
Adeel Syed <adeelsyed2004@gmail.com>
2024-10-06 00:44:00 -0400

    chat changed

--- app/backend/chat.py
+++ app/backend/chat.py
@@ -21,15 +21,15 @@ def process_image(image_url):
     
 def image_splicer(images):
     return None
-    
-def create_story(age, images):
+
+def create_story(age, frames, images):
     #storyboard = image_splicer(images)
     image = process_image(images)
 
     messages = [
         {'role': 'system', 'content': "You are writing children's stories for young children based on drawing that they give you. Please use the appropiate vocabulary and only output the story you make."},
         {'role': 'user', 'content': [
-            {'type': 'text', 'text': 'Based on this image, can you create a story that is easy for a 9 year old to read and will help them learn how to get better at reading. Note that in this story there is x amount of frames. Because this is supposed to represent a picture book, it should only be one description per frame (so if we specify and say that there is only one frame then there should only be one description). So that means that you have to output 1 description(s).Additionally, separate each description of each frame by an enter. At the beginning of your answer say the title of the story then do an enter then go into all the descriptions. Note that the vocabulary used should be appropriate for a 9 year old to understand. Even though it is a story it is possible for the story to be one sentece long because there is only one frame therefore only one description.'},
+            {'type': 'text', 'text': f'Based on this image, can you create a story that is easy for a {age} year old to read and will help them learn how to get better at reading. Note that in this story there is {frames} amount of frames. Because this is supposed to represent a picture book, it should only be one description per frame (so if we specify and say that there is only one frame then there should only be one description). So that means that you have to output {frames} description(s).Additionally, separate each description of each frame by an enter. At the beginning of your answer say the title of the story then do an enter then go into all the descriptions. Note that the vocabulary used should be appropriate for a {age} year old to understand. Even though it is a story it is possible for the story to be one sentece long because there is only one frame therefore only one description.'},
             {'type': 'image_url', 'image_url': {
                 'url': f"data:image/png;base64, {image}"
             }}
@@ -44,8 +44,13 @@ def create_story(age, images):
 
     return response.choices[0].message.content
 
-
-print(create_story(5, "assets/combined_image.png"))
+print("\n\n\n")
+print('This is the single picture example example: ')
+print(create_story(5, 1, "assets/single-frame.png"))
+print("\n\n\n")
+print('This is the Superman example: ')
+print(create_story(10, 3, "assets/combined_image.png"))
+print("\n")
 
 
 
